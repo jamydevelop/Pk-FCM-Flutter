27260dfd9742f730532194d02563cf4ed50aff84
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-08-05 19:13:18 +0800

    misc files

--- pk_fcm/lib/main.dart
+++ pk_fcm/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pk_fcm/firebase_msg.dart';
 import 'package:pk_fcm/firebase_options.dart';
 
 void main() async {
@@ -9,6 +10,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
+
+  await FirebaseMsg().initFCM();
 
   runApp(const MyApp());
 }
